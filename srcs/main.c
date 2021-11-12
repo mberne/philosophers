b5190b63c2d4bd1c2134f070a5280341151cc435
Manon Berne <mberne@z1r6p4.42lyon.fr>
2021-11-12 18:36:00 +0100

    all done, but program doesn't work

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mberne <mberne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:26:02 by mberne            #+#    #+#             */
-/*   Updated: 2021/11/12 18:19:16 by mberne           ###   ########lyon.fr   */
+/*   Updated: 2021/11/12 18:34:50 by mberne           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,22 +47,10 @@ void	create_philo(t_structs *s)
 	}
 }
 
-int	init_struct(t_structs *s, char **av)
+void	init_mutex(t_structs *s)
 {
 	int	i;
 
-	s->num_philo = ft_atoi(av[1]);
-	s->time_to_die = ft_atoi(av[2]);
-	s->time_to_eat = ft_atoi(av[3]);
-	s->time_to_sleep = ft_atoi(av[4]);
-	if (av[5])
-		s->num_eat = ft_atoi(av[5]);
-	else
-		s->num_eat = INT32_MAX;
-	s->philo = ft_calloc(sizeof(t_philo), s->num_philo);
-	s->fork = ft_calloc(sizeof(pthread_mutex_t), s->num_philo);
-	if (!s->philo || !s->fork)
-		return (-1);
 	i = 0;
 	while (i < s->num_philo)
 	{
@@ -72,11 +60,27 @@ int	init_struct(t_structs *s, char **av)
 		i++;
 	}
 	pthread_mutex_init(&s->speak, NULL);
-	s->wait_threads = 0;
-	s->death = 0;
+}
+
+int	init_struct(t_structs *s, char **av)
+{
+	s->num_philo = ft_atoi(av[1]);
+	s->time_to_die = ft_atoi(av[2]);
+	s->time_to_eat = ft_atoi(av[3]);
+	s->time_to_sleep = ft_atoi(av[4]);
+	if (av[5])
+		s->num_eat = ft_atoi(av[5]);
+	else
+		s->num_eat = INT32_MAX;
 	if (s->num_philo == 0 || s->time_to_die == 0 || s->time_to_eat == 0
 		|| s->time_to_sleep == 0 || s->num_eat == 0)
 		return (-1);
+	s->philo = ft_calloc(sizeof(t_philo), s->num_philo);
+	s->fork = ft_calloc(sizeof(pthread_mutex_t), s->num_philo);
+	if (!s->philo || !s->fork)
+		return (-1);
+	s->wait_threads = 0;
+	s->death = 0;
 	return (0);
 }
 
@@ -115,6 +119,7 @@ int	main(int ac, char **av)
 		free_struct(&s);
 		return (-1);
 	}
+	init_mutex(&s);
 	create_philo(&s);
 	free_struct(&s);
 	return (0);
